3221fc2cb92f7da8e730cfe2039f2e4acbe2133c
Sehar <sehar.sagarwala@gmail.com>
2019-01-14 14:36:53 -0600

    starting again

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,68 +1,18 @@
 import React, { Component } from 'react';
-import Home from './HomeComponent';
-import Dishdetail from './DishdetailComponent';
 import Menu from './MenuComponent';
-import { View, Platform } from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { DISHES } from '../shared/dishes';
 
-const MenuNavigator = createStackNavigator({
-        Menu: {screen: Menu},
-        Dishdetail: { screen: Dishdetail}
-}, {
-    initialRouteName: 'Menu',
-    navigationOptions:{
-        headerStyle:{
-            backgroundColor: '#512DA8'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle:{
-            color:'#fff'
-        }
-    }
-});
-
-const HomeNavigator = createStackNavigator({
-    Home: {screen: Home}
-}, {
-navigationOptions:{
-    headerStyle:{
-        backgroundColor: '#512DA8'
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle:{
-        color:'#fff'
-    }
-}
-});
-
-const MainNavigator = createDrawerNavigator({
-    Home: {
-        screen: HomeNavigator,
-        navigationOption:{
-            title: 'Home',
-            drawerLabel: 'Home'
-        }
-    },
-    Menu: {
-        screen: MenuNavigator,
-        navigationOption:{
-            title: 'Menu',
-            drawerLabel: 'Menu'
-        }
-    }
-},
-    {
-        drawerBackgroundColor:'#D1C4E9'
-    
-});
 class Main extends Component{
-    
+        constructor(props){
+            super(props);
+            this.state ={
+                dishes:DISHES
+            }
+        }
         render(){
             return (
-                <View style = {{ flex:1, paddingTop:Platform.OS === 'ios'? 0 :  Expo.Constants.statusBarHeight}} >
-                    <MainNavigator />
-                </View>
-            );
+                <Menu dishes={this.state.dishes} />
+            )
         }
 
 }
