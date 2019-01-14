6a40715dd1c7a5adc9317e92d6244d29a832ea73
Sehar <sehar.sagarwala@gmail.com>
2019-01-14 15:02:45 -0600

    components part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -6,12 +6,13 @@ function Menu (props){
 
     const renderMenuItem = ({item, index})=>{
 
-        return (
+        return ( 
             <ListItem 
                 key={index}
                 title={item.name}
                 subtitle={item.description}
                 hideChevron={true}
+                onPress={()=>props.onPress(item.id)}
                 leftAvatar={{ source: require('./images/uthappizza.png')}}
             />    
             );
